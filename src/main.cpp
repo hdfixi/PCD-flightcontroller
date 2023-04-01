29268ee33738de09747ca7fd9a3b7b048618584e
hdfixi <hdfixi0000@gmail.com>
2023-04-01 17:05:19 +0100

    before kartha

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
+#include<bits/stdc++.h>
 #include <SPI.h> 
 #include "nRF24L01.h"
 #include "RF24.h" 
-#include "BluetoothSerial.h"
+
 //bluetooth
-BluetoothSerial SerialBT;
+
 
 //DEBUG
+#define calib
 //#define DEBUG_MPU
+#define PID
 //#define DEBUG_NRF
-#define DEBUG_MOTORS_SPEED
+//#define DEBUG_MOTORS_SPEED
 //#define DEBUG_PID_VALUES
 //#define fixi
 //#define fixir
+//#define fixiy
 //#define fixi_motors
 //radio mid points 
 //Yaw:1473 Roll:1465 Pitch :1454
@@ -53,26 +57,27 @@ bool calibrate = true;
 void inline MPU_Angles_Avr(int16_t &rollavr, int16_t &pitchavr, int16_t &yawavr);
 void inline Measured_Roll_Pitch_Yaw(int16_t & Roll, int16_t & Pitch, int16_t & Yaw);
 
-#define PID_PITCH_kp 1
-#define PID_PITCH_ki 0
+#define PID_PITCH_kp 0
 #define PID_PITCH_kd 0
+#define PID_PITCH_ki 0
 
 #define PID_ROLL_kp PID_PITCH_kp
 #define PID_ROLL_kd PID_PITCH_kd
 #define PID_ROLL_ki PID_PITCH_ki
 
-#define PID_YAW_kp 0
-#define PID_YAW_kd 0
+#define PID_YAW_kp 1
+#define PID_YAW_kd 2
 #define PID_YAW_ki 0
 
 float previous_pitch_error = .0;
 float previous_roll_error = .0;
+float previous_yaw_error = .0;
 void Readytogo();
 void accCalib();
 void magnCalib();
 int16_t inline ExecutePitchPID(const int16_t& pitch_set_point, const int16_t& measured_pitch);
 int16_t inline ExecuteRollPID(const int16_t& roll_set_point, const int16_t& measured_roll);
-
+int16_t inline ExecuteYawPID(const int16_t& yaw_set_point, const int16_t& measured_yaw);
 void inline UpdateMotorsValues( const int16_t throttle, const int16_t pitch_pid_output, const int16_t roll_pid_output, const int16_t yaw_pid_output);
 
 #define DEBUG // for adding and removing debug code 
@@ -87,12 +92,13 @@ Servo MOTOR_3;
 RF24 receiver(12, 14, 26, 25, 27);
 //RF24 (CE, CSN, SCK, MISO, MOSI); //check link for class methods:https://maniacbug.github.io/RF24/classRF24.html
 
-const uint64_t p= 0xE8E8F0F0E1LL;//IMPORTANT: The same as in the transmitter
+const uint64_t p= 0xE8E8F0F0E2LL;//IMPORTANT: The same as in the transmitter
 
 int16_t values_received[4];
 
 void setup(void){
-
+//wireless upload 
+ //WiFi_setup(1); WebSerial_setup();  OTA_setup();
 #ifdef DEBUG
 Serial.begin(115200);
 #endif
@@ -142,7 +148,7 @@ Serial.println("Failed to initialize MPU9250.");
   {
   }
 }
-
+#ifdef calib
     accCalib();
 mpu.calibrateAccelGyro();
     digitalWrite(Bled,0);
@@ -150,15 +156,26 @@ mpu.calibrateAccelGyro();
  mpu.calibrateMag();
     digitalWrite(Gled,0);
     Readytogo();
-
+#endif
 
 }
 
-
 void loop(){
+if (mpu.update()) {
 Measured_Roll_Pitch_Yaw(Roll, Pitch, Yaw);
+    #ifdef DEBUG_MPU
+       Serial.print(", Yaw:");Serial.print(Yaw);
+      Serial.print(", Pitch:");Serial.print(Pitch); 
+      Serial.print(", Roll:");Serial.println(Roll);
+      //delay(50);
+    #endif
+    
+  }
+
 if(receiver.available()){
-receiver.read(values_received, sizeof(int16_t)*4);
+receiver.read(values_received, sizeof(values_received));
+digitalWrite(Bled,1);
+  digitalWrite(Rled,1);
   #ifdef DEBUG_NRF
       Serial.print("throtlle:");
   Serial.print(values_received[0]);
@@ -168,8 +185,10 @@ receiver.read(values_received, sizeof(int16_t)*4);
     Serial.print(values_received[2]);
    Serial.print("   Pitch:");
     Serial.print(values_received[3]);
-   Serial.println("   ");
+   Serial.print("   ");
+   int16_t PitchPIDOutput  = ExecutePitchPID(values_received[3], Pitch);
    #endif 
+
    //UpdateMotorsValues(values_received[0],PitchPIDOutput,0,0);
    #ifdef fixi_motors
 MOTOR_0.writeMicroseconds(values_received[0]);
@@ -177,9 +196,31 @@ MOTOR_1.writeMicroseconds(values_received[0]);
 MOTOR_2.writeMicroseconds(values_received[0]);
 MOTOR_3.writeMicroseconds(values_received[0]);
   #endif
+if(values_received[0]>1050){
+int16_t RollPIDOutput   = ExecuteRollPID(values_received[2], Roll);
+int16_t PitchPIDOutput  = ExecutePitchPID(values_received[3], Pitch);
+int16_t YawPIDOutput  = ExecuteYawPID(values_received[1], Yaw);
+#ifdef PID
+    Serial.print("RollPIDOutput:");
+  Serial.print(RollPIDOutput);
+   Serial.print("   PitchPIDOutput:");
+    Serial.print(PitchPIDOutput);
+   Serial.print("   YawPIDOutput :");
+    Serial.println(YawPIDOutput );
+#endif
+UpdateMotorsValues(values_received[0],PitchPIDOutput,RollPIDOutput ,YawPIDOutput);
+}
+else{
+  MOTOR_0.writeMicroseconds(MIN_THROTTLE);
+MOTOR_1.writeMicroseconds(MIN_THROTTLE);
+MOTOR_2.writeMicroseconds(MIN_THROTTLE);
+MOTOR_3.writeMicroseconds(MIN_THROTTLE);
+}
 }
 
-else{
+if(!receiver.available()){
+  digitalWrite(Bled,0);
+  digitalWrite(Rled,1);
   #ifdef DEBUG_NRF
     Serial.println(" No signal !  ");
   #endif 
@@ -188,20 +229,6 @@ MOTOR_1.writeMicroseconds(MIN_THROTTLE);
 MOTOR_2.writeMicroseconds(MIN_THROTTLE);
 MOTOR_3.writeMicroseconds(MIN_THROTTLE);
 }
-
-if (mpu.update()) {
-  
-    #ifdef DEBUG_MPU
-       Serial.print(", Yaw:");Serial.print(Yaw);
-      Serial.print(", Pitch:");Serial.print(Pitch); 
-      Serial.print(", Roll:");Serial.println(Roll);
-      //delay(50);
-    #endif
-    
-  }
-int16_t RollPIDOutput   = ExecuteRollPID(values_received[2], Roll);
-int16_t PitchPIDOutput  = ExecutePitchPID(values_received[3], Pitch);
-UpdateMotorsValues(values_received[0],PitchPIDOutput,RollPIDOutput ,0);
 
 //delay(10);
 
@@ -251,23 +278,26 @@ float integral = .0;
 float proportional;
 float derivative;
 long psp=map(pitch_set_point,1000,2000,-54,66);
-psp=max((long)-54,min(psp,(long)54));
+psp=max((long)-50,min(psp,(long)50));
+if(psp<=3&&psp>=-3)psp=0;
 error = static_cast<float>(psp- measured_pitch);
-//if(error<=5 && error>=-5)
-// error = 0;
+if(error<=3 && error>=-3)
+error = 0;
 proportional = PID_PITCH_kp * error;
-derivative = PID_PITCH_kd * (error - (previous_roll_error));
-integral += PID_PITCH_ki * (previous_roll_error + error)/2;
+derivative = PID_PITCH_kd * (error - (previous_pitch_error));
+integral += PID_PITCH_ki * (previous_pitch_error + error)/2;
 previous_pitch_error = error;
 #ifdef fixi
 Serial.print(" error:");
-Serial.print(proportional + integral + derivative);
+Serial.print(error );
+Serial.print(" previous_pitch_error:");
+Serial.print(previous_pitch_error );
 Serial.print(" mesured pitch:");
 Serial.print(measured_pitch);
 Serial.print(" pitch:");
 Serial.println(psp);
 #endif
-return static_cast<int16_t>(proportional + integral + derivative);
+return (proportional + integral + derivative);
 }
 
 int16_t inline ExecuteRollPID(const int16_t& roll_set_point, const int16_t& measured_roll) {
@@ -275,11 +305,14 @@ float error;
 float integral = .0;
 float proportional;
 float derivative;
-long rsp=map(roll_set_point,1000,2000,-55,65);
-rsp=max((long)-54,min(rsp,(long)54));
-error = rsp - measured_roll;
-//if(error<=10 && error>=-10)
-//error = 0;
+long rsp=map(roll_set_point,1000,2000,-52,68);
+rsp=max((long)-50,min(rsp,(long)50));
+if(rsp<=3&&rsp>=-3)rsp=0;
+
+error = static_cast<float>(rsp - measured_roll);
+
+if(error<=3 && error>=-3)
+error = 0;
 proportional = PID_ROLL_kp * error;
 derivative =PID_ROLL_kd* (error - (previous_roll_error));
 integral += PID_ROLL_ki* (previous_roll_error + error)/2;
@@ -287,19 +320,49 @@ previous_roll_error = error;
 #ifdef fixir
 Serial.print(" error:");
 Serial.print(proportional + integral + derivative);
-Serial.print(" mesured pitch:");
+Serial.print(" mesured roll:");
 Serial.print(measured_roll);
 Serial.print(" roll:");
 Serial.println(rsp);
 #endif
-return static_cast<int16_t>(proportional + integral + derivative);
+return (proportional + integral + derivative);
+}
+int16_t inline ExecuteYawPID(const int16_t& yaw_set_point, const int16_t& measured_yaw) {
+  float error=0;
+  static float integral = .0;
+  float proportional;
+  float derivative;
+  long ysp=map(yaw_set_point,1000,2000,-52,68);
+ysp=max((long)-50,min(ysp,(long)50));
+if(ysp<=3&&ysp>=-3)ysp=0;
+
+error = static_cast<float>(ysp - measured_yaw);
+
+if(error<=3 && error>=-3)error = 0;
+
+
+  proportional = PID_YAW_kp * error;
+  derivative = PID_YAW_kd * (error - (previous_yaw_error));
+  integral += PID_YAW_ki * (previous_yaw_error + error)/2;
+  
+    
+  previous_yaw_error = error;
+#ifdef fixiy
+Serial.print(" error:");
+Serial.print(proportional + integral + derivative);
+Serial.print(" mesured yaw:");
+Serial.print(measured_yaw);
+Serial.print(" yaw:");
+Serial.println(ysp);
+#endif
+  return (proportional + integral + derivative);
 }
 void inline UpdateMotorsValues( const int16_t throttle, const int16_t pitch_pid_output,
 const int16_t roll_pid_output, const int16_t yaw_pid_output) {
-long m0 = throttle - pitch_pid_output - roll_pid_output + yaw_pid_output;
-long m1 = throttle - pitch_pid_output + roll_pid_output - yaw_pid_output;
-long m2 = throttle + pitch_pid_output + roll_pid_output + yaw_pid_output;
-long m3 = throttle + pitch_pid_output - roll_pid_output - yaw_pid_output;
+long m0 = throttle - pitch_pid_output - roll_pid_output -yaw_pid_output;
+long m1 = throttle - pitch_pid_output + roll_pid_output + yaw_pid_output;
+long m2 = throttle + pitch_pid_output + roll_pid_output - yaw_pid_output;
+long m3 = throttle + pitch_pid_output - roll_pid_output + yaw_pid_output;
 
 
 
@@ -320,7 +383,7 @@ Serial.print("\n");
 #endif
 MOTOR_0.writeMicroseconds(m0);
 MOTOR_1.writeMicroseconds(m1);
- MOTOR_2.writeMicroseconds(m2);
+MOTOR_2.writeMicroseconds(m2);
 MOTOR_3.writeMicroseconds(m3);
 }
 void Readytogo(){
